a5aa455281be1d96f530f3616b2cf42bed352987
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-05 21:23:42 +0300

    added startCountdown method

--- CountdownApp/CountdownApp/MainViewController.swift
+++ CountdownApp/CountdownApp/MainViewController.swift
@@ -49,16 +49,43 @@ extension MainViewController {
             return cell
         })
         
-        // set type of animation on the data source
-        dataSource.defaultRowAnimation = .fade // .automatic
+//==============================================================================
+//        // set type of animation on the data source
+//        dataSource.defaultRowAnimation = .fade // .automatic
+//
+//        // setup snapshot
+//        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+//        // add sections
+//        snapshot.appendSections([.main])
+//        // add item's
+//        snapshot.appendItems([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+//        // apply changes to the dataSource
+//        dataSource.apply(snapshot, animatingDifferences: true)
+//==============================================================================
+        
+        startCountdown()
+    }
+    
+    private func startCountdown() {
+        if timer != nil {
+            timer.invalidate() // stop timer
+        }
+        // configure the timer
+        // set interval for countdown
+        // assign a method that gets called every second
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(decrementCounter), userInfo: nil, repeats: true)
+        // reset our startingInterval
+        startInterval = 10 // seconds
         
         // setup snapshot
         var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
-        // add sections
         snapshot.appendSections([.main])
-        // add item's
-        snapshot.appendItems([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        // apply changes to the dataSource
-        dataSource.apply(snapshot, animatingDifferences: true)
+        snapshot.appendItems([startInterval]) // start at 10
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    @objc
+    func decrementCounter() {
+        
     }
 }
