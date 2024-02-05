5aa3e3248a244d9856c17dc83686aaffd0db410d
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-05 20:49:13 +0300

    countdown initial commit

---
+++ CountdownApp/CountdownApp/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  CountdownApp
+//
+//  Created by Nikolai Maksimov on 05.02.2024.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
