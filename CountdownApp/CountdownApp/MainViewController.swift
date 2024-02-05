c68ab6b60327e23e426199f796dd1f5c0874d500
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-05 20:59:42 +0300

    properties added

--- CountdownApp/CountdownApp/MainViewController.swift
+++ CountdownApp/CountdownApp/MainViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
+    enum Section {
+        case main // one section for table view
+    }
     
+    // MARK: Private Properties
+    private var tableView: UITableView!
+    private var dataSource: UITableViewDiffableDataSource<Section, Int>!
+    
+    private var timer: Timer!
+    private var startInterval = 10 // seconds
+    
+    // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
