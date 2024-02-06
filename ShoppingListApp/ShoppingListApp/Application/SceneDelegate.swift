fed89bd584243c565a40bd590b3bf4aa5fcd7a95
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-06 20:50:38 +0300

    setup shopping list app

---
+++ ShoppingListApp/ShoppingListApp/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  ShoppingListApp
+//
+//  Created by Nikolai Maksimov on 06.02.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let navVC = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ ShoppingListApp/ShoppingListApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  ShoppingListApp
+//
+//  Created by Nikolai Maksimov on 06.02.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
