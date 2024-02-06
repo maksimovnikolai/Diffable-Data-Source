ba0fbe231d0128002dd0a6813d164a18a815f310
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-06 20:54:07 +0300

    added section categories

---
+++ ShoppingListApp/ShoppingListApp/Helpers/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  ShoppingListApp
+//
+//  Created by Nikolai Maksimov on 06.02.2024.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case running = "Running"
+    case technology = "Technology"
+    case education = "Education"
+    case health = "Health"
+    case household = "Household"
+    case triathlon = "Triathlon"
+    case shoppingCart = "Cart"
+}
